cd3083ee33c5caefdbe6d1444d4908c54fa7e7cd
tokelau <lobckina.nastya@yandex.ru>
2018-06-21 05:36:07 +0800

    Update Task.php

--- app/Task.php
+++ app/Task.php
@@ -1,77 +1,33 @@
 <?php
-
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class Task extends Model
 {
-   public $id;
-   public $topic;
-   public $type;
-   public $place;
-   public $date;
-   public $duration;
-   public $comment;
+   // public $id;
+   // public $topic;
+   // public $type;
+   // public $place;
+   // public $date;
+   // public $duration;
+   // public $comment;
 
-   public function __construct() {
-   		$topic = '123';
-   }
-
-   protected $errors = [];
-
-	protected static $rules = [
-	    'topic'    => 'Вы не ввели заголовок',
-	];
-
-	public function validate()
-    {
-        $this->errors = [];
-
-        foreach (static::$rules as $rule => $error_message)
-        {
-            if (empty($this->$rule))
-            {
-                $this->errors[$rule] = $error_message;
-            }
-        }
-
-        return empty($this->errors);
-    }
-
-    public function hasErrors()
-    {
-        return count($this->errors) > 0;
-    }
-
-    public function getErrors()
-    {
-        return $this->errors;
-    }
-
-    public function hasError($field)
-    {
-        return isset($this->errors[$field]);
-    }
-
-    public function getError($field)
-    {
-        return array_get($this->errors, $field, '');
-    }
-
+   public function __construct() { }
+   protected $fillable = ['topic', 'type', 'place', 'date', 'duration', 'comment'];
    public function Infill($data) //для заполнения в бд
     {
         //$this->name    = trim(array_get($data, 'name'));
       	$this->topic    = trim($data->topic);
-        $this->type   = trim(array_get($data, 'type'));
-        $this->place   = empty(trim(array_get($data, 'place'))) ? '-' : trim(array_get($data, 'place'));
-        $this->date = trim(array_get($data, 'date'));
-      	$this->duration = empty(trim(array_get($data, 'duration'))) ? '-' : trim(array_get($data, 'duration'));
-        $this->comment = empty(trim(array_get($data, 'comment'))) ? '-' : trim(array_get($data, 'comment'));
-
+        $this->type   = empty(trim($data->type)) ? 'Другое' : trim($data->type);
+        $this->date = date('Y-m-d H:i:s', strtotime(trim($data->date)));
+        $this->place   = empty(trim($data->place)) ? '-' : trim($data->place);
+      	$this->duration = empty(trim($data->duration)) ? '-' : trim($data->duration);
+        $this->comment = empty(trim($data->comment)) ? '-' : trim($data->comment);
         return $this;
     }
-
     public function toArray()
     {
         return [
@@ -84,18 +40,5 @@ class Task extends Model
         ];
     }
 
-    // public function save()
-    // {
-    //     // if ($this->validate())
-    //     // {
-    //     //   	$db = new DataBase;
-    //     //   	$db->insert($this->toArray());
-
-    //     //     return true;
-    //     // }
-    //     return true;
-    // }
-
     public $timestamps = false;
-
 }
